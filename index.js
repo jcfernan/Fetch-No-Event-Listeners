791092248ca814713fed459133d4125f6e4663ec
Carlos Fernandez <jcfernan@live.com>
2020-06-23 11:53:17 -0600

    Added ability to play around with events

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-fetch('http://bagel-api-fis.herokuapp.com/bagels')
-  .then(response => response.json())
-  .then(bagels => renderBagels(bagels))
- 
+//  document.addEventListener(event, function)  
+// Make sure all of functionality is loaded
+ document.addEventListener("DOMContentLoaded", () => {
+   console.log(document.getElementById('bagel-h1'))
 
+   const button = document.createElement('button')
+   button.innerText = "Load Bagels"
+   document.body.append(button)
 
-
-function renderBagels(bagels){
-  const bagelList = document.getElementById('bagel-list')
+   button.addEventListener('click', () => {
+    //  console.log('event', event.target.parentNode)
+    console.log('event', event.isTrusted)
+    fetchBagels()
+   })
   
-  bagels.map(bagel => {
-    const bagelLi = document.createElement('li')
-    bagelLi.innerText = bagel.type
-    bagelList.appendChild(bagelLi)
-  })
-}
+  function fetchBagels(){
+    fetch('http://bagel-api-fis.herokuapp.com/bagels')
+   .then(response => response.json())
+   .then(bagels => renderBagels(bagels))
+  }
+  
+ function renderBagels(bagels){
+   const bagelList = document.getElementById('bagel-list')
+   
+   bagels.map(bagel => {
+     const bagelLi = document.createElement('li')
+     bagelLi.innerText = bagel.type
+     bagelList.appendChild(bagelLi)
+
+    })
+  }
+
+ })
+
+
+
+
